7f073b1a42ece67570ed2dbe93f7b5b4261cc437
Leonre-MD <xinjaya86@gmail.com>
2024-07-17 09:37:33 +0700

    Add files via upload

---
+++ plugins/foto.js
@@ -0,0 +1,56 @@
+import fetch from 'node-fetch'
+const {
+    proto,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia
+  } = (await import('@adiwajshing/baileys')).default
+import { googleImage } from '@bochilteam/scraper'
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Use example ${usedPrefix}${command} anu`
+    m.reply(wait)
+    try {
+    const res = await googleImage(text)
+    let image = res.getRandom()
+    let link = image
+let msgs = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: `Nih Foto ${text}nya\nSource : Google`
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: wm
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+          hasMediaAttachment: false,
+          ...await prepareWAMessageMedia({ image: { url: link } }, { upload: conn.waUploadToServer })
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [{
+            "name": "quick_reply",
+              "buttonParamsJson": `{\"display_text\":\"Next Image ${text}\",\"id\":\".gimage ${text}\"}`
+            }],
+          })
+       })
+    }
+  }
+}, { quoted: m })
+
+return await conn.relayMessage(m.chat, msgs.message, {})
+} catch (e) {
+conn.sendFile(m.chat, eror, "anu.mp3", null, m, true, {
+		type: "audioMessage",
+		ptt: true,
+	})
+}}
+handler.help = ['gimage']
+handler.tags = ['downloader']
+handler.command = /^(gimage|googleimage|foto|gimg|googleimg|image)$/i
+handler.limit = true
+
+export default handler
